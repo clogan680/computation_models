d8696370c7d22fda38a4cb3e48f96885e4d3cce6
Chase Logan <clogan680@gmail.com>
2022-05-04 11:23:20 -0700

    Update cylinder_flow.py

--- external_flow/cylinder_flow.py
+++ external_flow/cylinder_flow.py
@@ -3,7 +3,6 @@ import numpy as np
 
 """
 Create Your Own Lattice Boltzmann Simulation (With Python)
-Philip Mocz (2020) Princeton Univeristy, @PMocz
 Simulate flow past cylinder
 for an isothermal fluid
 """
